7b9e5a8636e5c73b0ae213a7257270d88990429f
RJ Mohammad <rjmohammad@outlook.com>
2017-05-13 21:48:40 -0400

    set up passport, db and aweomse thingies

---
+++ server/facebook/fb.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clientID: '128210587730889',
+  clientSecret: 'a89dbd2732ebb786c954539fa8cd9986',
+  callbackURL: 'http://localhost:8080/auth/facebook/callback',
+  profileFields: ['id', 'displayName', 'user_photos', 'email']
+}
