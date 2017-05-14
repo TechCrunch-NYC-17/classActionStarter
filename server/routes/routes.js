4a4232d495173e1c6da5b324b70027efe743bb2c
RJ Mohammad <rjmohammad@outlook.com>
2017-05-13 20:03:19 -0400

    basic set up

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,55 @@
 const lawsuits = require('../controllers/lawsuits');
-
-module.exports = (app) => {
+const token = require('../jwt/jwt');
+module.exports = (app, passport) => {
   app.get('/fetchlawsuits', lawsuits.fetch);
-}
+  app.post('/signup', passport.authenticate('local-signup', {
+    failureRedirect: '/',
+    failureFlash: true
+  }),
+    (req, res) => {
+      res.send({ token: passport.token, user: passport.user, picture: `https://robohash.org/${passport.user.displayname}` });
+    });
+
+  app.post('/login', passport.authenticate('local-login', {
+    failureRedirect: '/#/'
+  }),
+    (req, res) => {
+      res.send({ token: passport.token, user: passport.user, picture: `https://robohash.org/${passport.user.displayname}` });
+    });
+
+  app.post('/tokenauth', (req, res) => {
+    if (token.verifyToken(req.body.token) !== undefined) {
+      res.send({ validToken: true });
+    } else {
+      res.send({ validToken: false });
+    }
+  });
+
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    authType: 'rerequest',
+    failureRedirect: '/login'
+  }), (req, res) => {
+    passport.user = req.user;
+    passport.token = token.tokenGenerator(req.user.id);
+    res.redirect('/#/auth');
+  });
+
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/', (req, res) => {
+    res.redirect('/#/');
+  });
+
+  app.get('/auth', (req, res) => {
+    res.redirect('/#/auth');
+  });
+
+  app.get('/get-info', (req, res) => {
+    res.send({ token: passport.token, user: passport.user, picture: passport.photo });
+  });
+};
--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ var bodyParser = require('body-parser')
 var morgan = require('morgan')
 const path = require('path')
 const cors = require('cors')
+const session = require('express-session')
+const bcrypt = require('bcrypt-nodejs')
 const routes = require('./routes/routes.js')
-
+const passport = require('passport')
+const FacebookStrategy = require('passport-facebook').Strategy
+const config = require('../config/fb')
+const LocalStrategy = require('passport-local').Strategy
+const db = require('./models/db')
 var app = express()
 var port = process.env.port || 8080
 app.use(morgan('dev'))
