86460ceca9e14e4f29cb6fbc164d79bd45f348bb
Sojung Park <sojungko@gmail.com>
2017-05-13 20:02:30 -0400

    Fix routing issues

--- client/index.jsx
+++ client/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { HashRouter as Router, Route, hashHistory } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
+
 import App from './containers/App';
+import Form from './containers/Form'
 
 const store = createStore(
   rootReducer,
@@ -14,9 +17,14 @@ const store = createStore(
 
 ReactDOM.render(
   <MuiThemeProvider>
-    <Provider store={store}>
-      <Router history={hashHistory} routes={routes} />
-    </Provider>
+    <Router history={hashHistory}>
+      <Provider store={store}>
+        <div>
+          <Route exact path='/' component={App} />
+          <Route path='/form' component={Form} />
+        </div>
+      </Provider>
+    </Router>
   </MuiThemeProvider>
   , document.getElementById('root'));
 
